a8dd44f4a4f832dea2d2b1f5b425bae70d1addea
Jhonata Demuner <jhonata.demuner@gmail.com>
2025-06-26 21:03:29 -0300

    refactor: standardize to plural endpoint paths

--- src/main/kotlin/com/architer/controller/challenge/ChallengeController.kt
+++ src/main/kotlin/com/architer/controller/challenge/ChallengeController.kt
@@ -15,27 +15,27 @@ class ChallengeController(
     private val challengeService: ChallengeService
 ) {
 
-    @PostMapping("/v1/challenge")
+    @PostMapping("/v1/challenges")
     fun create(@RequestBody challenge: ChallengeCreateDTO): ResponseEntity<ChallengeDTO> {
         return ResponseEntity.status(HttpStatus.CREATED).body(challengeService.create(challenge))
     }
 
-    @GetMapping("/v1/challenge")
+    @GetMapping("/v1/challenges")
     fun findAll(@RequestParam page: Int = 0, @RequestParam size: Int = 10): ResponseEntity<List<ChallengeDTO>> {
         return ResponseEntity.status(HttpStatus.OK).body(challengeService.findAll(page, size))
     }
 
-    @GetMapping("/v1/challenge/{id}")
+    @GetMapping("/v1/challenges/{id}")
     fun findById(@PathVariable id: UUID): ResponseEntity<ChallengeDTO> {
         return ResponseEntity.status(HttpStatus.OK).body(challengeService.findById(id))
     }
 
-    @PutMapping("/v1/challenge")
+    @PutMapping("/v1/challenges")
     fun update(@RequestBody challenge: ChallengeUpdateDTO): ResponseEntity<ChallengeDTO> {
         return ResponseEntity.status(HttpStatus.OK).body(challengeService.update(challenge))
     }
 
-    @DeleteMapping("/v1/challenge/{id}")
+    @DeleteMapping("/v1/challenges/{id}")
     fun delete(@PathVariable id: UUID): ResponseEntity<Void> {
         challengeService.delete(id)
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build()
