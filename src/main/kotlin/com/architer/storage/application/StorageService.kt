5b32bad11055130c20997c93a69acb4081ba1f3f
Jhonata Demuner <jhonata.demuner@gmail.com>
2025-08-26 23:53:26 -0300

    feat: add storage domain

---
+++ src/main/kotlin/com/architer/storage/application/StorageService.kt
@@ -0,0 +1,25 @@
+package com.architer.storage.application
+
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.model.ObjectMetadata
+import com.amazonaws.services.s3.model.PutObjectRequest
+import com.architer.storage.infrastructure.config.MinioProperties
+import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+import java.util.UUID
+
+@Service
+class StorageService(
+    private val minioProperties: MinioProperties,
+    private val amazonS3: AmazonS3
+) {
+    fun uploadInterviewDiagram(interviewId: UUID, image: MultipartFile): String {
+        val key = "interviews/diagrams/$interviewId.png"
+        val metadata = ObjectMetadata().apply {
+            contentLength = image.size
+            contentType = image.contentType ?: "image/png"
+        }
+        amazonS3.putObject(PutObjectRequest(minioProperties.bucket, key, image.inputStream, metadata))
+        return amazonS3.getUrl(minioProperties.bucket, key).toString()
+    }
+}
